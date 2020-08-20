3930d5c216646a5c7aefa7be088fe2684b124556
Łukasz Jankowski <Lukasz.Jankowski@partner.commerzbank.com>
2020-08-20 20:01:48 +0200

    Save file and monitor procces

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <fstream> 
 #include <windows.h>
+#include <tchar.h>
 
+/**
+ * @file Untitled1
+ * @author Lukasz Jankowski
+ * @date 29.06.2020
+ * @brief EX 2 - Systemy operacyjne II, cwiczenie 2 - LAB
+ 	Program nie spelnia oczekiwan prowadzacego - tzn. nie jest w jednym pliku i nie moglem wykorzystac
+ 	parametru CreateFile jako mutexa. Wolalem oddac jakas swoja prace w terminie niz pozostac z niczym.
+ 	Jezeli ma Pan zastrzezenia i moglby Pan podpowiedziec jak to zrealizowac w jednym pliku prosilbym
+ 	o krotka informacje na maila. 
+ 	Dziekuje i pozdrawiam.
+ */
 
 HANDLE createFile(){
 
@@ -50,26 +62,35 @@ void CreateAndSave(){
 int main(int argc, TCHAR *argv[], TCHAR *envp[]){
 	
 	PROCESS_INFORMATION pi1; 
-	char cmd1[80] = "Untitled1 -mode CreateAndSave";
+	std::string cmd1 = "MonitorProcess.exe file";
+ 	STARTUPINFO si1 ;
+ 	ZeroMemory( &si1, sizeof(si1) );
+    si1.cb = sizeof(si1);
+    ZeroMemory( &pi1, sizeof(pi1) );
+	if(CreateProcess(0,(char*) cmd1.c_str(), 0,0,TRUE, CREATE_NEW_CONSOLE, 0,0, &si1, &pi1))
+    { 
+       
+    }  
+    else
+    {
+        std::cout<<("The process could not be started...")<<std::endl;
+    }	
 	
- 	STARTUPINFO si1 = {
- 		sizeof(si1), NULL, NULL,(LPTSTR) "createAndSave", 500, 370,
- 		420, 310, 
- 		0, 0,
- 		0x0d, 
- 		STARTF_USEFILLATTRIBUTE | STARTF_USEPOSITION | STARTF_USESIZE,
- 		0, 0, NULL, NULL, NULL }; 
- 		
- 		
- 		CreateProcess(0,cmd1, NULL,NULL,0, CREATE_NEW_CONSOLE, NULL,NULL, &si1, &pi1);
+	PROCESS_INFORMATION pi2; 
+	std::string cmd2 = "MonitorProcess.exe monitorProcess";
+ 	STARTUPINFO si2;
+ 	ZeroMemory( &si2, sizeof(si2) );
+    si2.cb = sizeof(si2);
+    ZeroMemory( &pi2, sizeof(pi2) );
+	if(CreateProcess(0,(char*) cmd2.c_str(), 0,0,TRUE, CREATE_NEW_CONSOLE, 0,0, &si2, &pi2)){
+	
+	}
+	else
+    {
+        std::cout<<("The process could not be started...")<<std::endl;
+    }
+		
  	
- 		std::string cas= "CreateAndSave";
- 		for(int i=0;i<argc;i++){
- 			if(argv[i]==(TCHAR*)cas.c_str()){
- 			CreateAndSave();
-		 	}
-	 	}
- 		
  	
 	
 }
